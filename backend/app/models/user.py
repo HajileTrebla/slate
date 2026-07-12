fdd25367e0e8580028ef77971d12e20a0f973e2c
Eli <eli.quinio17@gmail.com>
2026-07-12 22:34:03 +0800

    Cleanup and Add password and timestamps to user table

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,13 +1,43 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import DateTime, String, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from datetime import datetime
 
 from app.core.db import Base
 
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True, nullable=False)
-    email = Column(String, unique=True, nullable=False)
 
-    first_name = Column(String, nullable=True)
-    last_name = Column(String, nullable=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    username: Mapped[str] = mapped_column(
+            String(50),
+            unique=True,
+            nullable=False,
+            index=True,
+        )
+
+    email: Mapped[str | None] = mapped_column(
+            String(255),
+            unique=True,
+            nullable=True,
+        )
+
+    password: Mapped[str] = mapped_column(
+            String(255),
+            nullable=False,
+        )
+
+    created_at: Mapped[datetime] = mapped_column(
+            DateTime(timezone=True),
+            server_default=func.now(),
+            nullable=False,
+        )
+
+    updated_at: Mapped[datetime] = mapped_column(
+            DateTime(timezone=True),
+            server_default=func.now(),
+            onupdate=func.now(),
+            nullable=False,
+    )
