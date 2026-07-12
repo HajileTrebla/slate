7b5162fc535fb890e8971fed53166505a39bf4c0
Eli <eli.quinio17@gmail.com>
2026-07-12 22:35:13 +0800

    Update user schema

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,19 +1,23 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
+from datetime import datetime
 
 
 class UserCreate(BaseModel):
     username: str
-    email: EmailStr
-    first_name: str
-    last_name: str
+    email: EmailStr | None
+    password: str = Field(..., min_length=8, max_length=255)
+    created_at: datetime
+    updated_at: datetime
 
 
 class UserResponse(BaseModel):
     id: int
     username: str
-    email: EmailStr
-    first_name: str | None
-    last_name: str | None
+    email: EmailStr | None
+    password: str
+    created_at: datetime
+    updated_at: datetime
+
     model_config = {
         "from_attributes": True
     }
